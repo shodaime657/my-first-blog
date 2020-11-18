61443b7c7ab5baf796bc4636c28c40e01ad6b488
Brayan villalobos <brayan.alberto@uabc.edu.mx>
2020-11-17 20:11:47 -0800

    se agrego la funcionalidad de comentarios

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
